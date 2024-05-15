a49a02591ccf9e8f47f22dacc6ab63a8809755d5
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-05-15 23:42:16 +0200

    Add files via upload

---
+++ Nadawca/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Nadawca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wyslane");
+            Console.WriteLine("Nacisnij przycisk");
+            Console.ReadKey();
+
+            string queueName = "queueZadanie3";
+
+            var factory = new ConnectionFactory();
+            factory.UserName = "guest";
+            factory.Password = "guest";
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+
+            IConnection conn = factory.CreateConnection();
+            IModel channel = conn.CreateModel();
+            channel.QueueDeclare(queue: queueName,
+                                      durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                      arguments: null);
+
+            var channelProperties = channel.CreateBasicProperties();
+            channelProperties.Headers = new Dictionary<string, object>();
+            {
+                channelProperties.Headers.Add("queueName", queueName);
+                channelProperties.Headers.Add("Autor", "Bartosz Kolakowski");
+            }
+            for (int i = 1; i <= 10; i++)
+            {
+                string message = $"Wyslano {i}";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "",
+                                        routingKey: queueName,
+                                        basicProperties: channelProperties,
+                                        body: body);
+
+                Console.WriteLine($"Wyslano: '{message}'");
+            }
+
+            Console.WriteLine("Nacisnij przycisk");
+            Console.ReadKey();
+
+            channel.Close();
+            conn.Close();
+        }
+    }
+}
---
+++ Odbiorca/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Odbiorca");
+        Console.WriteLine("Nacisnij przycisk");
+        Console.ReadKey();
+        string queueName = "queueZadanie3";
+        var factory = new ConnectionFactory
+        {
+            UserName = "guest",
+            Password = "guest",
+            HostName = "localhost",
+            VirtualHost = "/"
+        };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: queueName,
+                                  durable: false,
+                                  exclusive: false,
+                                 autoDelete: false,
+                                  arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Otrzymano: '{message}' od {queueName}");
+                var headers = ea.BasicProperties.Headers;
+                if (headers != null)
+                {
+                    foreach (var header in headers)
+                    {
+                        Console.WriteLine($"Naglowek: {header.Key} = {Encoding.UTF8.GetString((byte[])header.Value)}");
+                    }
+                }
+            };
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine("Nacisnij przycisk");
+            Console.ReadKey();
+        }
+    }
+}
